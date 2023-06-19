c2962a5545247a0cba9e51374c4a15b2caf46ab5
Sabbir Hossain <sabbirsopnil07@gmail.com>
2023-06-19 14:56:31 +0600

    Initial Commit

---
+++ NewSwiftUIPieChart/NewSwiftUIPieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSwiftUIPieChartApp.swift
+//  NewSwiftUIPieChart
+//
+//  Created by Sopnil Sohan on 19/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NewSwiftUIPieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
